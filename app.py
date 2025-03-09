df24d90cabbe80f86c9dca67597d5bb99fa714d6
Jukkrit <80025293+tonjk@users.noreply.github.com>
2025-03-10 01:09:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def health_check():
 def test_api():
     return 'Testing API is completed.', 200
 
+@app.route('test_post',methods=['POST'])
+def test_post():
+    data = request.json
+    sentence = data.get('sentence')
+    response = str(sentence) + ' ' + Thank you for your request. ^^
+    return jsonify({"response": response}), 200
+    
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
