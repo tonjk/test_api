6d3c7453302412cf1b4773d734d464603f832b49
Jukkrit <80025293+tonjk@users.noreply.github.com>
2025-03-11 23:02:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,jsonify,abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import Configuration # ApiClient, MessagingApi, ReplyMessageRequest, TextMessage, ImageMessage, FlexMessage
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage, ImageMessage, FlexMessage
 from linebot.v3.webhooks import MessageEvent, TextMessageContent, LocationMessageContent
 import os, json, requests
 from dotenv import load_dotenv
