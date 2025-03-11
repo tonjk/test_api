6ec361e3ac96ff2753d1c7c56637f104bf3813b9
Jukkrit <80025293+tonjk@users.noreply.github.com>
2025-03-11 18:43:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
 from flask import Flask,request,jsonify
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration # ApiClient, MessagingApi, ReplyMessageRequest, TextMessage, ImageMessage, FlexMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent, LocationMessageContent
 
 app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+# Initialize LINE API and Webhook handler
+configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=["POST"])
+def callback(oa_id):
+    """Handle webhook requests from Line."""
+    # Get the signature and body
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+def send_flex_message(user_id, response_text):
+    token = LINE_OA_CONFIGS[current_oa]['channel_access_token']
+    headers = {'Content-Type': 'application/json',
+               'Authorization': f'Bearer {token}'}
+    data = {"to": user_id,
+            "messages": [{"type": "text",
+                          "text": response_text}
+                        ]
+            }
+    response = requests.post('https://api.line.me/v2/bot/message/push', headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print(f"Failed to send message. Status: {response.status_code}, Response: {response.text}")
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    user_id = event.source.user_id
+    # message_text = event.message.text
+    send_flex_message(user_id, "thank you :)")
+
 
 # check health path
 @app.route('/healthy',methods=['GET'])
